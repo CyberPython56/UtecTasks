20343f10f54a2e1498518f5665067a1c5d3ceb1f
Roman <romant56@mail.ru>
2025-02-25 15:07:57 +0300

    add the result output to the terminal

--- cli.py
+++ cli.py
@@ -1,27 +1,27 @@
 import click
 from tasks import get_response
+from celery.result import AsyncResult
 
 
-def correct_ip(ip: str):
+def correct_ip(ip: str):  # Проверка корректности ip
     return all(x.isdigit() or x == '.' for x in ip) and len(ip.split('.')) == 4 and ip.find('..') == -1 \
         and all(0 <= int(x) <= 255 for x in ip.split('.'))
 
 
-def correct_port(port: str):
+def correct_port(port: str):  # Проверка корректности порта
     return all(x.isdigit() for x in port) and 0 <= int(port) <= 65535
 
 
 @click.command()
 @click.argument('ip')
 @click.argument('port')
-def get_response_task(ip: str, port: str):
+def get_response_task(ip: str, port: str):  # Основная функция запуска процесса
     if correct_ip(ip) and correct_port(port):
-        res = get_response.delay(ip, port)
-        click.echo(f'Задача запущена: {res.id}')
+        result = get_response.delay(ip, port)
+        click.echo(f'Результат GET-запроса:\n{result.get(timeout=10)}')
     else:
         click.echo('[Error] Ошибка в корректности аргументов')
 
 
-# 127.0.0.1
 if __name__ == '__main__':
     get_response_task()
--- tasks.py
+++ tasks.py
@@ -1,13 +1,19 @@
 from celery import Celery
 import requests
 
-app = Celery('tasks')
+app = Celery('tasks', backend='rpc://', broker='amqp://guest@localhost//')
+
 
 # Выполнение GET-запроса к ip и порту
 @app.task
 def get_response(ip, port):
     try:
-        return f'[OK] {requests.get(f'http://{ip}:{port}').text}'
+        response = requests.get(f'http://{ip}:{port}')
+        if response.status_code > 399:  # Проверка кода ответа
+            return f'[Error] {response.reason}'  # Возвращаем причину ошибки
+        return response.text
     except Exception as e:
         return f"[Error] {e}"
 
+# celery -A tasks worker --loglevel=INFO
+# celery -A tasks flower --broker=amqp://guest:guest@${RABBITMQ_SERVICE_SERVICE_HOST:localhost}:5672//
